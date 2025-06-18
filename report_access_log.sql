5cd6df3db6121deb824879b71f7880f0a8e454bd
Mrigank Sachan <msachan562000@gmail.com>
2025-06-18 10:17:02 +0530

    Update report_access_log.sql

--- report_access_log.sql
+++ report_access_log.sql
@@ -7,6 +7,6 @@ CREATE TABLE `report_access_log` (
  `report_name` varchar (64),
  `parameters` varchar (256),
  `user_name` varchar (64),
- `generated_timestamp` date
+ `generated_timestamp` date,
  PRIMARY KEY (`id`)
 );
